1beb866f0e740fe3698bcfe9f78c9d35a4d58103
Artem-Elyashevich <elyashevich.artem06@gmail.com>
2023-03-11 21:58:52 +0300

    add redux

---
+++ backend/config.py
@@ -0,0 +1,8 @@
+path = '..\\frontend\\src\\static\\bookimg'
+default_img = '..\\frontend\\src\\static\\bookimg\\1_1[1].bmp'
+
+default_user = '..\\frontend\\src\\static\\bookimg\\default-user-image[1].png'
+path_user = '..\\frontend\\src\\static\\userimg'
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'bmp'}
+UPLOAD_FOLDER = path
